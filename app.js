93ecc15c5e4c7bf82785e1652f630ea0744019ea
Hein Htet <heinhtet25944@gmail.com>
2019-12-12 17:33:04 +0630

    Insert

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get("/api/user/role", (req, res) => {
       console.log("connected")
     );
     const selectRole =
-    "SELECT tbl_role.roleName,tbl_role.active,tbl_role.remark,tbl_employee.createdDate,tbl_employee.employeeName FROM tbl_user JOIN tbl_employee ON tbl_user.employeeId=tbl_employee.employeeId JOIN tbl_role ON tbl_user.roleId=tbl_role.roleId";
+    "SELECT tbl_role.roleId,tbl_role.roleName,tbl_role.active,tbl_role.remark,tbl_role.createdDate,tbl_employee.employeeName FROM tbl_user JOIN tbl_employee ON tbl_user.employeeId=tbl_employee.employeeId JOIN tbl_role ON tbl_user.roleId=tbl_role.roleId";
     dbcon.connect(err => {
       if (err) throw err;
       dbcon.query(selectRole, (err, result, fields) => {
@@ -116,8 +116,72 @@ app.get("/api/user/role", (req, res) => {
         const data = result
         res.json(response({ success: true, payload: data }));
       });
-    });
+    });  
+});
+
+
+app.post("/api/user/role/addRole", (req, res) => {
   
+  const dbcon = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "restaurant"
+    },
+    console.log("connected")
+  );
+  const InsertRoleName=req.body.newData.RoleName
+  const InsertRemark=req.body.newData.Remark
+  const InsertActive=req.body.newData.active
+  const InsertDate=req.body.newData.createdDate
+  console.log(req.body);
+  
+const checkDuplicate=`Select * from tbl_role where roleName='${"Hein"}' and roleId<>0`
+  const selectRole =
+  `INSERT INTO tbl_role (roleName, active, remark,createBy, createdDate) VALUES ('${InsertRoleName}', ${InsertActive==="true"?1:0}, '${InsertRemark}', 1, '${InsertDate}')`
+  dbcon.connect(err => {
+    if (err) throw err;
+    dbcon.query(selectRole, (err, result, fields) => {
+      if (err) throw err;
+      
+      const data = result
+      res.json(response({ success: true, payload: data }));
+    });
+  });
+
+});
+
+
+app.post("/api/user/role/updateRole", (req, res) => {
+  
+  const dbcon = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "restaurant"
+    },
+  );
+  const UpdateRoleName=req.body.newData.RoleName
+  const UpdateRemark=req.body.newData.Remark
+  const UpdateActive=req.body.newData.active
+  const UpdateId=req.body.newData.roleId
+
+  console.log(req.body.newData);
+  
+  const selectRole =
+  `UPDATE tbl_role SET roleName = '${UpdateRoleName}',remark='${UpdateRemark}',createBy=1,active=${UpdateActive==="true"?1:0} WHERE roleId=${UpdateId}`
+  dbcon.connect(err => {
+    if (err) throw err;
+    dbcon.query(selectRole, (err, result, fields) => {
+      if (err) throw err;
+      
+      const data = result
+      res.json(response({ success: true, payload: data }));
+    });
+  });
+
 });
 
 // app.use(express.urlencoded({extended:false}))
